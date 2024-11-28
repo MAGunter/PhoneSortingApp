a1ebed0e282a82f1aecc997b11a28691a085821c
Codemedy <maratarslan0@gmail.com>
2024-11-29 01:17:28 +0500

    final version to present, but open for modify

--- src/main/java/com/barista/maker/numbersortingapp/controller/SortingController.java
+++ src/main/java/com/barista/maker/numbersortingapp/controller/SortingController.java
@@ -1,5 +1,6 @@
 package com.barista.maker.numbersortingapp.controller;
 
+import com.barista.maker.numbersortingapp.sort.QuickSort;
 import lombok.RequiredArgsConstructor;
 import com.barista.maker.numbersortingapp.entity.Person;
 import com.barista.maker.numbersortingapp.repository.PersonRepository;
@@ -9,7 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 @Controller
 @RequestMapping("/api/sorting")
@@ -17,16 +21,42 @@ import java.util.List;
 public class SortingController {
     private final RadixSort radixSort;
     private final PersonRepository personRepository;
+    private final QuickSort quickSort;
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
 
     @GetMapping("/radix")
     public String radixSort(Model model){
-        List<Person> list = personRepository.findAll();
-        int[] numbers = list.stream().mapToInt(person -> person.getNumber().intValue()).toArray();
-        radixSort.executeRadixSort(numbers);
-        model.addAttribute("persons", list.stream()
-                .sorted((p1, p2) -> Long.compare(p1.getNumber(), p2.getNumber()))
-                .toList());
+        return sort(model, radixSort::executeRadixSort, "radix");
+    }
 
-        return "radix";
+    @GetMapping("/quick")
+    public String quickSort(Model model){
+        return sort(model, quickSort::executeQuickSort, "quick");
+    }
+
+    private String sort(Model model, Consumer<int[]> sortMethod, String viewName) {
+        List<Person> list = personRepository.findAll();
+        long startTime = System.currentTimeMillis();
+        int[] numbers = list.stream().mapToInt(person -> person.getNumber().intValue()).toArray();
+        sortMethod.accept(numbers);
+        List<Person> sortedPersons = Arrays.stream(numbers)
+                .mapToObj(number -> list.stream()
+                        .filter(person -> person.getNumber().intValue() == number)
+                        .findFirst()
+                        .orElse(null))
+                .filter(Objects::nonNull)
+                .toList();
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        model.addAttribute("duration", duration);
+        model.addAttribute("persons", sortedPersons);
+
+        return viewName;
     }
 }
